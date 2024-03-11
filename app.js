21c2fd636f7329a26dc4b8b6d4bc6bf349368b5b
Abaljerind <abaljerinda@gmail.com>
2024-03-11 17:09:44 +0700

    create function typeEffect

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// membuat function agar ketika layar di mobile version dan di klik icon bar nya akan muncul tampilan menu nya
+// function untuk layar pada mobile size agar saat di klik icon bar nya, maka akan muncul tampilan menu nav nya
 const elements = document.querySelectorAll(".hidden");
 
 function dropdown() {
@@ -6,3 +6,36 @@ function dropdown() {
     element.classList.toggle("hidden");
   });
 }
+
+// function untuk efek mengetik
+const dynamicText = document.querySelector("h1.greeting span");
+const words = ["AbalJerind", "a Front-end Developer"];
+
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+const typeEffect = () => {
+  const currentWord = words[wordIndex];
+  const currentChar = currentWord.substring(0, charIndex);
+  dynamicText.textContent = currentChar;
+  dynamicText.classList.add("stop-blinking");
+
+  if (!isDeleting && charIndex < currentWord.length) {
+    // jika kondisi diatas bernilai true maka tampilkan huruf selanjutnya
+    charIndex++;
+    setTimeout(typeEffect, 200);
+  } else if (isDeleting && charIndex > 0) {
+    // jika kondisi diatas bernilai true maka hapus huruf sebelumnya
+    charIndex--;
+    setTimeout(typeEffect, 150);
+  } else {
+    // jika variable wordIndex sudah dihapus, lanjut ke word / isi array words selanjutnya.
+    isDeleting = !isDeleting;
+    dynamicText.classList.remove("stop-blinking");
+    wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
+    setTimeout(typeEffect, 1500);
+  }
+};
+
+typeEffect();
